99cad6a8551852db49defa1b30ff64a3a2827b55
Travis Clarke <travis.m.clarke@gmail.com>
2018-09-12 14:47:10 -0700

    add index.js skeleton

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const port = process.env.PORT || 3000;
+const mongoose = require("mongoose");
+
+console.log("TODO");
+
+process.exit(1);
